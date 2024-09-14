92255bddc82881ccf534f71381ea30026e364a6b
Taher Shihadeh <taher@unixwars.com>
2024-09-14 12:24:31 +0100

    Cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,3 @@
-function main() {
-    function makeImages(size) {
-        return [...Array(size).keys()].map(i => {
-            let src = 'assets/img/emblem_' +('0' + i).slice(-2)+'.webp';
-            let img = document.createElement('img');
-            img.src = src;
-            img.alt = "Random Sigil";
-            img.title = "Emblem";
-            return img;
-    })};    
-    function switchImage() {
-        images[idx].classList.toggle('show');
-        idx = Math.floor(Math.random() * images.length);
-        images[idx].classList.toggle('show');
-    }
-    let div = document.getElementById('content');
-    let images = makeImages(49);
-    let idx = Math.floor(Math.random() * images.length);
-    images[idx].classList.toggle('show');
-    div.append(...images);
-    setInterval(switchImage, 5000);
+function main(params) {
 }
-
-// window.onload = (event) => main();
+window.onload = (event) => main();
